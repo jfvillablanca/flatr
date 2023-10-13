b3df67ce9c4b195fdf71e9629307d24b6a29ac56
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-10-13 13:17:38 +0800

    feat: add file path validation

--- src/cli.rs
+++ src/cli.rs
@@ -1,3 +1,5 @@
+use std::fs::File;
+
 use clap::Parser;
 
 /// Flattenize JSON files
@@ -5,6 +7,13 @@ use clap::Parser;
 #[command(author, version, about, long_about = None)]
 pub struct Args {
     /// Path to JSON file
-    #[arg(short, long)]
+    #[arg(short, long, value_parser = validate_json_file_path)]
     pub file_path: String,
+}
+
+fn validate_json_file_path(path: &str) -> Result<(), String> {
+    match File::open(path) {
+        Ok(_) => Ok(()),
+        Err(e) => Err(format!("{}", e)),
+    }
 }
