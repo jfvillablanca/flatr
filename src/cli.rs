6c05d30274615b84f3eddea9299eadbfbd6123f2
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-10-13 15:49:35 +0800

    test: add integration test for path validator

--- src/cli.rs
+++ src/cli.rs
@@ -17,3 +17,27 @@ fn validate_json_file_path(path: &str) -> Result<(), String> {
         Err(e) => Err(format!("{}", e)),
     }
 }
+// Integration test
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::fs::File;
+    use std::io::Write;
+
+    #[test]
+    fn test_validate_json_file_path() {
+        // Create a temporary JSON file for testing
+        let test_file_name = "test.json";
+        let json_data = r#"{"key": "value"}"#;
+        let mut file = File::create(&test_file_name).expect("Unable to create test file");
+
+        file.write_all(json_data.as_bytes())
+            .expect("Unable to write to test file");
+
+        // Use the function to validate the JSON file path
+        assert_eq!(validate_json_file_path(&test_file_name), Ok(()));
+
+        // Clean up the test file
+        std::fs::remove_file(&test_file_name).expect("Unable to remove test file");
+    }
+}
